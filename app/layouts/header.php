00f267b60782be470b8b3ce61afe7272fc43d638
João Victor <olegarioo.dev@gmail.com>
2025-02-14 20:44:30 -0300

    Alterações no header

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -7,10 +7,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POKESHOP</title>
+    <link rel="stylesheet" href="../../public/assets/css/style.css";
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+20&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <h1>Bem-vindo ao POKESHOP</h1>
-        <!-- Adicione mais elementos do cabeçalho aqui -->
+
+<header>
+<nav class="container-menu">
+        <a class="menu-titulo" href="#">
+            <img class="menu-img" src="../../public/assets/img/logo.webp" alt="PokeShop" height="50px" width="50px">
+            POKESHOP
+        </a>
+
+        <button class="btn btn-outline-primary">
+            <p>user</p>
+            <i class="bi bi-person"></i>
+        </button>
+        
+        <button class="btn btn-outline-primary">
+            <p>mercado</p>
+            <i class="bi bi-shop"></i>
+        </button>
+    </nav>
+
     </header>
