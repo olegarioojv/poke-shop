66d28ac80431dbb2bbd08c28d20282779f93eacf
João Victor <olegarioo.dev@gmail.com>
2025-02-14 23:08:04 -0300

    Alterando o menu

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -7,29 +7,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/assets/css/style.css";
+    <link rel="stylesheet" href="../../public/assets/css/style.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jersey+20&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
 </head>
 <body>
 
 <header>
-<nav class="container-menu">
-        <a class="menu-titulo" href="#">
+    <nav class="container-menu navbar-expand-lg ">
+
+        <a class="menu-titulo">
             <img class="menu-img" src="../../public/assets/img/logo.webp" alt="PokeShop" height="50px" width="50px">
             POKESHOP
         </a>
-
-        <button class="btn btn-outline-primary">
-            <p>user</p>
-            <i class="bi bi-person"></i>
-        </button>
         
-        <button class="btn btn-outline-primary">
-            <p>mercado</p>
-            <i class="bi bi-shop"></i>
-        </button>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <button class="btn btn-outline-dark">
+                        <i class="bi bi-person mr-2"></i>
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-outline-dark">
+                        <i class="bi bi-shop mr-2"></i>
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-outline-dark">
+                        <span>R$ 0</span>
+                    </button>
+                </li>
+            </ul>
+        </div>
     </nav>
+</header>
 
-    </header>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
